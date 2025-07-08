eae6a9a2969d1755fa8231e129d78c9c87f0fa1f
iohxcl <iohxchile@gmail.com>
2025-07-08 15:37:21 -0400

    Habilitar CORS para frontend local

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
-# backend/main.py
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def home():
-    return {"mensaje": "¡Marca Chancho API funcionando correctamente!"}
+def root():
+    return {"mensaje": "¡Marca Chancho API está viva!"}
 
 @app.get("/buscar")
 def buscar_producto(q: str):
